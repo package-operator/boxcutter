b081bc7513868e2430f02fc31e0e0225d2431687
Nico Schieder <nschieder@redhat.com>
2025-01-22 10:52:25 +0100

    Revisions stuff++

--- test/reference/reference.go
+++ test/reference/reference.go
@@ -3,23 +3,27 @@ package reference
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"strings"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/rest"
 	"pkg.package-operator.run/boxcutter/machinery"
 	"pkg.package-operator.run/boxcutter/machinery/ownerhandling"
-	"pkg.package-operator.run/boxcutter/machinery/types"
+	bctypes "pkg.package-operator.run/boxcutter/machinery/types"
 	"pkg.package-operator.run/boxcutter/machinery/validation"
 	"pkg.package-operator.run/boxcutter/managedcache"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
@@ -27,8 +31,9 @@ import (
 )
 
 const (
-	fieldOwner   = "boxcutter.test"
-	systemPrefix = "boxcutter.test"
+	fieldOwner     = "boxcutter.test"
+	systemPrefix   = "boxcutter.test"
+	cacheFinalizer = "boxcutter.test/cache"
 )
 
 type Reference struct {
@@ -48,13 +53,13 @@ func NewReference(
 
 // Starts the reference controller and blocks until the context is cancelled.
 func (r *Reference) Start(ctx context.Context) error {
-	// cm := &corev1.ConfigMap{}
 	mgr, err := ctrl.NewManager(r.restConfig, ctrl.Options{
 		WebhookServer: webhook.NewServer(webhook.Options{Port: 9443}),
 		Cache: cache.Options{
 			ByObject: map[client.Object]cache.ByObject{
 				&corev1.ConfigMap{}: {
 					Label: labels.SelectorFromSet(labels.Set{
+						// Label selector so our main controller does only react to specific ConfigMaps.
 						"package-operator.run/test-type": "Revision",
 					}),
 				},
@@ -140,7 +145,40 @@ func (c *CMRevisionReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 
 	re := machinery.NewRevisionEngine(pe, rval, c.client)
 
-	rres, err := re.Reconcile(ctx, c.toRevision(revisionCM))
+	rev, err := c.toRevision(revisionCM)
+	if err != nil {
+		return res, fmt.Errorf("converting CM to revision: %w", err)
+	}
+
+	if !revisionCM.DeletionTimestamp.IsZero() {
+		tres, err := re.Teardown(ctx, rev)
+		if err != nil {
+			return res, fmt.Errorf("revision teardown: %w", err)
+		}
+
+		fmt.Println("-----------")
+		fmt.Println(tres.String())
+
+		if !tres.IsComplete() {
+			return res, nil
+		}
+
+		if err := c.cache.Free(ctx, revisionCM); err != nil {
+			return res, fmt.Errorf("freeing cache: %w", err)
+		}
+
+		if err := c.removeFinalizer(ctx, revisionCM, cacheFinalizer); err != nil {
+			return res, err
+		}
+
+		return res, nil
+	}
+
+	if err := c.ensureFinalizer(ctx, revisionCM, cacheFinalizer); err != nil {
+		return res, err
+	}
+
+	rres, err := re.Reconcile(ctx, rev)
 	if err != nil {
 		return res, fmt.Errorf("revision reconcile: %w", err)
 	}
@@ -151,48 +189,108 @@ func (c *CMRevisionReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 	return res, nil
 }
 
-func (c *CMRevisionReconciler) toRevision(cm *corev1.ConfigMap) types.Revision {
-	return &types.RevisionStandin{
+func (c *CMRevisionReconciler) ensureFinalizer(
+	ctx context.Context, obj client.Object, finalizer string,
+) error {
+	if controllerutil.ContainsFinalizer(obj, finalizer) {
+		return nil
+	}
+
+	controllerutil.AddFinalizer(obj, finalizer)
+	patch := map[string]any{
+		"metadata": map[string]any{
+			"resourceVersion": obj.GetResourceVersion(),
+			"finalizers":      obj.GetFinalizers(),
+		},
+	}
+	patchJSON, err := json.Marshal(patch)
+	if err != nil {
+		return fmt.Errorf("marshalling patch to remove finalizer: %w", err)
+	}
+
+	if err := c.client.Patch(ctx, obj, client.RawPatch(types.MergePatchType, patchJSON)); err != nil {
+		return fmt.Errorf("adding finalizer: %w", err)
+	}
+	return nil
+}
+
+func (c *CMRevisionReconciler) removeFinalizer(
+	ctx context.Context, obj client.Object, finalizer string,
+) error {
+	if !controllerutil.ContainsFinalizer(obj, finalizer) {
+		return nil
+	}
+
+	controllerutil.RemoveFinalizer(obj, finalizer)
+
+	patch := map[string]any{
+		"metadata": map[string]any{
+			"resourceVersion": obj.GetResourceVersion(),
+			"finalizers":      obj.GetFinalizers(),
+		},
+	}
+	patchJSON, err := json.Marshal(patch)
+	if err != nil {
+		return fmt.Errorf("marshalling patch to remove finalizer: %w", err)
+	}
+	if err := c.client.Patch(ctx, obj, client.RawPatch(types.MergePatchType, patchJSON)); err != nil {
+		return fmt.Errorf("removing finalizer: %w", err)
+	}
+	return nil
+}
+
+const cmPhasesKey = "phases"
+
+func (c *CMRevisionReconciler) toRevision(cm *corev1.ConfigMap) (bctypes.Revision, error) {
+	var phases []string
+	objects := map[string][]unstructured.Unstructured{}
+	for k, v := range cm.Data {
+		if k == cmPhasesKey {
+			if err := json.Unmarshal([]byte(v), &phases); err != nil {
+				return nil, fmt.Errorf("json unmarshal key %s: %w", k, err)
+			}
+			continue
+		}
+
+		parts := strings.SplitN(k, "_", 2)
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("malformed object key")
+		}
+		phase := parts[0]
+
+		obj := unstructured.Unstructured{}
+		if err := json.Unmarshal([]byte(v), &obj); err != nil {
+			return nil, fmt.Errorf("json unmarshal key %s: %w", k, err)
+		}
+
+		// Default namespace to the owners namespace
+		if len(obj.GetNamespace()) == 0 {
+			obj.SetNamespace(
+				cm.GetNamespace())
+		}
+
+		objects[phase] = append(objects[phase], obj)
+	}
+
+	rev := &bctypes.RevisionStandin{
 		Name:     cm.Name,
 		Owner:    cm,
 		Revision: 1,
-		Phases: []types.Phase{
-			&types.PhaseStandin{
-				Name: "phase-1",
-				Objects: []types.PhaseObject{
-					{
-						Object: &unstructured.Unstructured{
-							Object: map[string]interface{}{
-								"apiVersion": "v1",
-								"kind":       "ConfigMap",
-								"metadata": map[string]interface{}{
-									"name":      "test-rev-obj-1",
-									"namespace": "default",
-								},
-							},
-						},
-					},
-				},
-			},
-			&types.PhaseStandin{
-				Name: "phase-2",
-				Objects: []types.PhaseObject{
-					{
-						Object: &unstructured.Unstructured{
-							Object: map[string]interface{}{
-								"apiVersion": "v1",
-								"kind":       "ConfigMap",
-								"metadata": map[string]interface{}{
-									"name":      "test-rev-obj-2",
-									"namespace": "default",
-								},
-							},
-						},
-					},
-				},
-			},
-		},
 	}
+
+	for _, phase := range phases {
+		p := &bctypes.PhaseStandin{
+			Name: phase,
+		}
+		for _, obj := range objects[phase] {
+			p.Objects = append(p.Objects, bctypes.PhaseObject{
+				Object: &obj,
+			})
+		}
+		rev.Phases = append(rev.Phases, p)
+	}
+
+	return rev, nil
 }
 
 func (c *CMRevisionReconciler) SetupWithManager(mgr ctrl.Manager) error {
