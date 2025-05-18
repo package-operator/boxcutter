e35fed402c702c0e397dad82b5b925163b893cef
Josh Gwosdz <jgwosdz@redhat.com>
2025-05-18 18:42:02 +0200

    fix: rename error to ErrEmptyKindOrVersion
    Signed-off-by: Josh Gwosdz <jgwosdz@redhat.com>

--- managedcache/gvk.go
+++ managedcache/gvk.go
@@ -8,13 +8,13 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-// ErrNonEmptyKindOrVersion is returned when either kind or version of the given object are empty.
-var ErrNonEmptyKindOrVersion = errors.New("object must have non-empty kind and version")
+// ErrEmptyKindOrVersion is returned when either kind or version of the given object are empty.
+var ErrEmptyKindOrVersion = errors.New("object must have non-empty kind and version")
 
 func gvkForObject(obj runtime.Object) (schema.GroupVersionKind, error) {
 	gvk := obj.GetObjectKind().GroupVersionKind()
 	if gvk.Kind == "" || gvk.Version == "" {
-		return schema.GroupVersionKind{}, fmt.Errorf("%w: %s", ErrNonEmptyKindOrVersion, gvk)
+		return schema.GroupVersionKind{}, fmt.Errorf("%w: %s", ErrEmptyKindOrVersion, gvk)
 	}
 
 	return gvk, nil
